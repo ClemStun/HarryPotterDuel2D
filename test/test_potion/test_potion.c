8eab73fbbd44264e2353f8057ddfcd46ad9de70f
ClemStun <stundeveloppe@gmail.com>
2021-04-19 15:09:53 +0200

    modif test potion

--- test/test_potion/test_potion.c
+++ test/test_potion/test_potion.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../../lib/menus.h"
 #include "../../lib/potions.h"
-#include "../../lib/ingredients.h"
 
-static
 int GetPotionIndex(int potion){
 
     int i;
@@ -17,81 +16,66 @@ int GetPotionIndex(int potion){
 
 }
 
-static
-int MakePotion(int potion){
+void MakePotion(){
 
+    int ingredient;
+    int potion = 0;
+    int restant = 3;
     int i;
+
+    while(restant > 0){
+
+        ingredient = 0;
+
+        printf("Ajoutez %i ingredients !\n", restant);
+
+        while(ingredient < 1 || ingredient > 3){
+            printf("\nIngrédients disponible :\n");
+            printf("1 - Racinde d'asphodele\n");
+            printf("2 - Ecrain de licorne\n");
+            printf("3 - Mandragore\n");
+
+            scanf("%i", &ingredient);
+            if(ingredient < 1 || ingredient > 3)
+                printf("Cet ingredient n'existe pas !\n\n");
+        }
+
+        potion = potion * 10 + ingredient;
+        restant --;
+
+        printf("Ingredients de la potion : %i\n", potion);
+    }
 
     i = GetPotionIndex(potion);
     if(i < 0)
         printf("Cette potions n'existe pas \n");
-    else{
+    else
         printf("Vous avez confectionné une potion: %s\n", nomPotions[i]);
-        potions_unlock[i] = 1;
-    }
 
-    return i;
 }
 
-static
-int AfficherPotion(){
-    int i, j, n, in, noPotion = 1;
-    for(i = 0; i < NB_POT; i++){
-        if(potions_unlock[i] == 1){
-            noPotion = 0;
-            printf("%s : \n\t", nomPotions[i]);
-            n = potions[i];
-            for(j = 100; j > 0; j = j/10){
-              in = n/j-1;
-              printf("%s ", ingredients[in]);
-              n %= j;
-            }
-            printf("\n");
-        }
+void menu(){
+
+    int choix = 1;
+
+    while(choix){
+
+        printf("Lancement de la création ...\n");
+        MakePotion();
+
+        printf("Voulez-vous faire un autre test ? (0/1)");
+        scanf("%i", &choix);
+
     }
-    if(noPotion){
-        printf("Vous n'avez pas encore créer de potion !\n");
-        return 1;
-    }else{
-        return 0;
-    }
+
 }
 
-int main(){
 
-    printf("-- TEST POTION --\n");
+int main(void){
 
-    printf("-- Test affichage potion pas débloqué --\n");
-    if(AfficherPotion() == 1)
-        printf("OK !\n");
-    else
-        printf("KO !\n");
-
-    printf("-- Test sur potion qui existe --\n");
-    if(MakePotion(123) >= 0)
-        printf("OK !\n");
-    else
-        printf("KO !\n");
-    
-    printf("-- Test sur potion avec qui n'existe pas --\n");
-    if(MakePotion(421) < 0)
-        printf("OK !\n");
-    else
-        printf("KO !\n");
-
-    printf("-- Test sur potion avec ingrédients inexistant --\n");
-    if(MakePotion(421) < 0)
-        printf("OK !\n");
-    else
-        printf("KO !\n");
-
-    printf("-- Test affichage potion débloqué --\n");
-    if(AfficherPotion() == 0)
-        printf("OK !\n");
-    else
-        printf("KO !\n");
-
+    printf("-- TEST CREATION POTION --\n");
+    menu();
     printf("-- FIN DE TEST --\n");
 
-    return 0;
+    return EXIT_SUCCESS;
 }
